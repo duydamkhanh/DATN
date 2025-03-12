933438207155a6828369ae86cbef282ab81492f6
Sharknam <namcr1542@gmail.com>
2025-03-12 20:43:35 +0700

    fix:oulet layout

--- fe/src/routes/_layout.tsx
+++ fe/src/routes/_layout.tsx
@@ -1,10 +1,22 @@
-// import ChatBot from '@/components/ChatBot';
+import { useEffect } from 'react';
+import { createFileRoute, Outlet, useLocation } from '@tanstack/react-router';
 import Footer from '@/components/footer';
 import Header from '@/components/header';
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+
+const ScrollToTop = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+};
+
 export const Route = createFileRoute('/_layout')({
   component: () => (
     <div>
+      <ScrollToTop />
       <div>
         <Header />
         <Outlet />
