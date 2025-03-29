2803984b4931a98dbd33efbe7d017ae5510f4de0
duyhung <hungdao891@gmail.com>
2025-03-29 12:32:19 +0700

    fix: integrate api comment list

--- fe/src/data/Comment/useCommentList.tsx
+++ fe/src/data/Comment/useCommentList.tsx
@@ -1,28 +1,22 @@
-import { useQuery } from '@tanstack/react-query';
-import { QUERY_KEY } from '../stores/key';
 import instance from '@/api/axiosIntance';
+import { useQuery } from '@tanstack/react-query';
 
-// Hàm fetch bình luận từ API theo productId
-export const fetchComments = async (productId: string) => {
-  try {
-    // Gọi API lấy tất cả bình luận cho sản phẩm
-    const res = await instance.get<{ data: Comment[] }>(`/comments/product/${productId}`);
-    
-    if (res.status !== 200) {
-      throw new Error(`Error fetching comments: ${res.status}`);
-    }
+const fetchComments = async (productIds: string[]) => {
+  if (!Array.isArray(productIds) || productIds.length === 0) return [];
 
-    return res.data; // Dữ liệu bao gồm cả bình luận và thông tin người dùng
-  } catch (error: any) {
-    throw new Error('Error fetching comments');
-  }
+  const idsString = productIds.join(',');
+  const response = await instance.get(
+    `/comments/products?productIds=${idsString}`
+  );
+  return response.data;
 };
 
-// Hook `useFetchComments` sử dụng `useQuery` để gọi API lấy bình luận theo productId
-export const useFetchComments = (productId: string) => {
+const useProductComments = (productIds: string[]) => {
   return useQuery({
-    queryKey: [QUERY_KEY.FETCH_COMMENT_BY_PRODUCT, productId], // Sử dụng productId để làm key
-    queryFn: () => fetchComments(productId),
-    enabled: !!productId, // Chỉ gọi API khi productId đã có giá trị
+    queryKey: ['comments', productIds],
+    queryFn: () => fetchComments(productIds),
+    enabled: productIds.length > 0,
   });
 };
+
+export default useProductComments;
