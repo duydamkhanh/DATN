d617fa0c87526c73f78c0835fc9af7f8ef08b820
dam duy <duy942002@gmail.com>
2025-04-04 23:48:15 +0700

    add avatar

--- fe/src/routes/dashboard/_layout/index.tsx
+++ fe/src/routes/dashboard/_layout/index.tsx
@@ -1,9 +1,9 @@
 import Header from '@/components/layoutAdmin/header/header';
-// import OrderList from '@/components/layoutAdmin/orderdashboard/dashboard-order';
-// import MyBarChart from '@/components/layoutAdmin/statistics/BarChart';
-// import DashboardOverview from '@/components/layoutAdmin/statistics/LineChar';
-// import PieChartExample from '@/components/layoutAdmin/statistics/PiaChart';
-// import ToDoList from '@/components/layoutAdmin/statistics/toDoList';
+import OrderList from '@/components/layoutAdmin/orderdashboard/dashboard-order';
+import MyBarChart from '@/components/layoutAdmin/statistics/BarChart';
+import DashboardOverview from '@/components/layoutAdmin/statistics/LineChar';
+import PieChartExample from '@/components/layoutAdmin/statistics/PiaChart';
+import ToDoList from '@/components/layoutAdmin/statistics/toDoList';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/dashboard/_layout/')({
@@ -15,12 +15,12 @@ function ThongKe() {
     <div className="h-screen overflow-y-auto">
       <Header title="Dashboard" pathname="/" />
       <div className="overflow-y-hidden">
-        {/* <ToDoList /> */}
-        {/* <DashboardOverview /> */}
-        {/* <OrderList /> */}
+        <ToDoList />
+        <DashboardOverview />
+        <OrderList />
         <div className="flex justify-between overflow-y-hidden">
-          <div className="flex-1">{/* <MyBarChart /> */}</div>
-          <div className="flex-1">{/* <PieChartExample /> */}</div>
+          <div className="flex-1"><MyBarChart /></div>
+          <div className="flex-1"><PieChartExample /></div>
         </div>
       </div>
     </div>
